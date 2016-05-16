4a17ad56342962a46399248dfccc646417514b5e
Tyler Daines <tyler.daines@uplynk.com>
2016-05-16 14:40:02 -0700

    Initial commit of sample apps

---
+++ microsoft/uwp/cs/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using Uplynk.MediaSource;
+
+namespace SamplePlayer
+{
+    public sealed partial class MainPage
+    {
+        private UplynkMediaSource uplynkMediaSource;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeAdaptiveMediaSource(new Uri("http://content.uplynk.com/52ab86c3f6c74d6abf7f162204d51609.m3u8"));
+        }
+
+        private async void InitializeAdaptiveMediaSource(Uri uri)
+        {
+            UplynkMediaSourceCreationResult result = await UplynkMediaSource.CreateFromUriAsync(uri);
+
+            if (result.Status == UplynkMediaSourceCreationStatus.Success)
+            {
+                uplynkMediaSource = result.UplynkMediaSource;
+                uplynkMediaSource.AssetEntered += UplynkMediaSource_AssetEntered;
+                uplynkMediaSource.SegmentEntered += UplynkMediaSource_SegmentEntered;
+
+                mediaElementPlayer.SetPlaybackSource(uplynkMediaSource.MediaPlaybackItem);
+            }
+        }
+        private void UplynkMediaSource_AssetEntered(UplynkMediaSource sender, AssetEnteredEventArgs args)
+        {
+            Debug.WriteLine($"Now playing: {args.Asset.Description}");
+        }
+
+        private void UplynkMediaSource_SegmentEntered(UplynkMediaSource sender, SegmentEnteredEventArgs args)
+        {
+            Debug.WriteLine($"{args.Segment.Asset.Description}: {args.Segment.StartTime} -> {args.Segment.EndTime}");
+        }
+    }
+}
---
+++ microsoft/uwp/js/js/main.js
@@ -0,0 +1,99 @@
+﻿(function () {
+    'use strict';
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    var url = null;
+    var uplynk = Uplynk.MediaSource;
+    var mediaSource = null;
+    var mediaPlaybackItem = null;
+    var uplynkMediaSource = null;
+
+    function attachMediaSource() {
+
+        if (mediaPlaybackItem != null) {
+            var vid = document.getElementById("video_player");
+
+            var playlist = new Windows.Media.Playback.MediaPlaybackList();
+            playlist.items.append(mediaPlaybackItem);
+
+            vid.src = URL.createObjectURL(playlist, { oneTimeOnly: true });
+        }
+    }
+
+    function onAssetEntered(event) {
+        console.log("now playing: " + event.asset.description);
+    }
+
+    function getTimeString(milliseconds) {
+
+        var time = new Date(milliseconds);
+        var minutes = time.getMinutes();
+        var seconds = time.getSeconds();
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        return minutes + ":" + seconds;
+    }
+
+    function onSegmentEntered(event) {
+
+        var segment = event.segment;
+
+        var start = getTimeString(segment.startTime);
+        var end = getTimeString(segment.endTime);
+
+        console.log(segment.asset.description + ": " + start + " -> " + end);
+    }
+
+    function onMediaSourceCreated(result) {
+
+        if (result.status === Uplynk.MediaSource.UplynkMediaSourceCreationStatus.success) {
+
+            mediaSource = result.uplynkMediaSource.mediaSource;
+            mediaPlaybackItem = result.uplynkMediaSource.mediaPlaybackItem;
+            uplynkMediaSource = result.uplynkMediaSource;
+            uplynkMediaSource.addEventListener("assetentered", onAssetEntered, false);
+            uplynkMediaSource.addEventListener("segmententered", onSegmentEntered, false);
+
+            attachMediaSource();
+        }
+    }
+
+    function loadMediaSourceFromUri(urlString) {
+
+        var vid = document.getElementById("video_player");
+
+        url = new Windows.Foundation.Uri(urlString);
+
+        mediaSource = null;
+        vid.removeAttribute("src");
+
+        WinJS.log && WinJS.log("Creating AdaptiveMediaSource for url: " + url, "sample", "status");
+
+        uplynk.UplynkMediaSource.createFromUriAsync(url).done(
+            function completed(result) {
+                onMediaSourceCreated(result);
+            });
+    }
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+
+            args.setPromise(WinJS.UI.processAll().then(function () {
+                loadMediaSourceFromUri("http://content.uplynk.com/52ab86c3f6c74d6abf7f162204d51609.m3u8");
+            }));
+        }
+    };
+
+    app.oncheckpoint = function (args) { };
+
+    app.start();
+}());
