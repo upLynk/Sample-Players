4709f35dd47c48cc6a24d3b5c828fec94ca9df58
Chad Spencer <chad.spencer@verizondigitalmedia.com>
2016-07-22 11:48:23 -0600

    Remove 'remove title' code that doesn't work

--- android/app/src/main/java/com/uplynk/sampleplayer/VideoActivity.java
+++ android/app/src/main/java/com/uplynk/sampleplayer/VideoActivity.java
@@ -93,10 +93,6 @@ public class VideoActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // remove title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_video);
 
